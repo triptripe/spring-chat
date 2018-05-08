6ea35fee035d81a00206f189bd820e9b228394cf
Vadim <vrmargiev@yandex.ru>
2018-05-08 03:48:43 +0300

    add image post

--- src/main/java/clientChat/Client.java
+++ src/main/java/clientChat/Client.java
@@ -1,34 +1,18 @@
 package clientChat;
 
-
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
-
-import java.io.IOException;
-import java.util.Scanner;
 
 public class Client {
-    static Long id;
-
-    public static void main(String[] args) throws IOException {
-        WebSocketClient webSocketClient = new StandardWebSocketClient();
-        WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
-        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-        stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
-        id = new Scanner(System.in).nextLong();
-        String urlChat = "ws://192.168.180.58:8080/hello";
-        handler2 sessionHandler = new handler2();
-        sessionHandler.addListener(new Reciever());
-        sessionHandler.addListener(new Sender());
-        stompClient.connect(urlChat, sessionHandler);
 
 
-        while (true) ;
-//        new Scanner(System.in).nextLine(); //Don't close immediately.
+    /**
+     * post entity
+     */
+
+    public static String IP = "192.168.180.58:8008";
+    public static void main(String[] args) {
+        System.out.println(Commands.getEvents().get(0).getImage().length);
     }
 }
